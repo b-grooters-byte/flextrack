73df3e6b5c34afc9b2f73ad2a049cbadcab16cbc
bytetrail <brandon.grooters@gmail.com>
2016-02-14 12:56:11 -0600

    corrected for undefined slope

--- src/Geometry.cpp
+++ src/Geometry.cpp
@@ -3,11 +3,9 @@
 #include <cmath>
 #include <iostream>
 
-namespace ByteTrail
-{
+namespace ByteTrail {
 
-    double Point::Distance(const Point & other) const
-    {
+    double Point::Distance(const Point & other) const {
         return std::sqrt((x - other.x) * (x - other.x) + (y - other.y) * (y - other.y));
     }
 
@@ -20,16 +18,17 @@ namespace ByteTrail
     //          (x2 - x1)
     // the point passed in the parameter list is considered as x2, y2
     //-----------------------------------------------------------------------------------
-    double Point::Slope(const Point & other) const
-    {
+    double Point::Slope(const Point & other) const {
         double cy = (other.y - y);
         double cx = (other.x - x);
 
-        return cy / cx;
+        if( cx != 0)
+            return cy / cx;
+        else
+            return std::nan("UNDEFINED");
     }
 
-    std::unique_ptr<Point> Point::GetPointFrom(const Point & midpoint) const
-    {
+    std::unique_ptr<Point> Point::GetPointFrom(const Point & midpoint) const {
         std::unique_ptr<Point> point = std::unique_ptr<Point>(new Point());
 
         double cy = (midpoint.y - y);
@@ -43,13 +42,11 @@ namespace ByteTrail
     }
 
 
-    bool Rect::Contains(const Point & p ) const
-    {
+    bool Rect::Contains(const Point & p ) const {
         return Contains(p.x, p.y);
     }
 
-    bool Rect::Contains(double x, double y) const
-    {
+    bool Rect::Contains(double x, double y) const {
         return ((x >= this->x) && (x <= (this->x + width)) && (y >= this->y) && (y <= (this->y+height)));
     }
 
